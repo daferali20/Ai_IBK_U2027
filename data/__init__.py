a2d91126dd5629b35286d6484e4565152b4a4ac4
daferali20 <31751340+daferali20@users.noreply.github.com>
2026-08-07 17:04:41 +0300

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,12 @@
+# data/__init__.py
+"""
+معالجة البيانات وجلبها
+"""
 
+from .fetcher import DataFetcher
+from .indicators import TechnicalIndicators
+
+__all__ = [
+    'DataFetcher',
+    'TechnicalIndicators'
+]
